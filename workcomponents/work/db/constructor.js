9795e647c89dadbc15ffec22b36468905a7c264d
pdhoward <patrick.howard@hotmail.com>
2018-06-13 15:41:51 -0400

    remove extra directories

--- workcomponents/work/db/constructor.js
+++
@@ -1,9 +0,0 @@
-
-// constructor.js
-exports.constructor = constructor => o => {
-  const proto = Object.assign({},
-    Object.getPrototypeOf(o),
-    { constructor }
-  );
-  return Object.assign(Object.create(proto), o);
-};
--- workcomponents/work/with-constructor.js
+++
@@ -1,9 +0,0 @@
-
-// with-constructor.js
-exports.withConstructor = constructor => o => {
-  const proto = Object.assign({},
-    Object.getPrototypeOf(o),
-    { constructor }
-  );
-  return Object.assign(Object.create(proto), o);
-};
