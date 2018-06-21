4fbb93172f2155c0fd9cf8001cfd2f5b98cf3dce
pdhoward <patrick.howard@hotmail.com>
2018-06-21 12:40:08 -0400

    refactor for test gen bot

--- zserver/index.js
+++ zserver/index.js
@@ -27,41 +27,85 @@ app.use(logger("dev"));
 
 
 //////////////////////////////////////////////////////
-////////// Register and Config Routes ///////////////
+////////// Test Data Generator Bot   ///////////////
 ////////////////////////////////////////////////////
 
-const sms =      express.Router();
-const db =       express.Router();
-const web =      express.Router();
-const auth =     express.Router();
-const errs =     express.Router();
-const unk =      express.Router();
-const help =     express.Router();
 
-require('../routes/db')(db);
-require('../routes/web')(web);
-require('../routes/unk')(unk);
-require('../routes/error')(errs);
-require('../routes/help')(help);
+function checkURL() {
 
-//////////////////////////////////////////////////////////////////////////
-///////////////////////////// API CATALOGUE /////////////////////////////
-////////////////////////////////////////////////////////////////////////
+  console.log("");
+  console.log("");
 
-// help
-app.get('/', help)
-// web > twilio > text
-app.use('/api/web', web)
-// db api
-app.use('/api/db', db)
-// 404
-app.use(unk)
-// error handling
-app.use(errs)
+  // If invalid url.
+  if (invalidurl) {
 
-// server
-let port = process.env.PORT || process.env.localport;
+    console.log("###############################################");
+    console.log("");
+    console.log("URL Entered Not Valid");
+    console.log("");
+    console.log("###############################################");
 
-app.listen(port, () => {
-  console.log(b('listening on port '), port)
-});
+    // Exit
+    process.exit();
+  }
+
+  // After performing the "check", the next set of configs is intiated
+  buildConfig();
+
+}
+
+
+// This function holds the process logic for configuring test data bases
+function buildConfig() {
+
+  // We create a list prompt. Specifying that the user must pick a random number between 1 and 5.
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "userGuess",
+      message: "Try to stay alive! Guess a number between [1-5]",
+      choices: ["1", "2", "3", "4", "5"]
+    }
+
+  ]).then(function(guess) {
+
+    // If the user is still alive or the zombie is still alive
+    if (userHealth > 0 || zombieHealth > 0) {
+
+      // Assign a random damage value for the round.
+      var damage = Math.floor(Math.random() * 5) + 1;
+
+      // The zombie should choose a random number.
+      var zombieNum = Math.floor((Math.random() * 5)) + 1;
+      console.log("");
+      console.log("");
+      console.log("Zombie rolled " + zombieNum);
+
+      // If the user's guess matches the number then...
+      if (zombieNum === parseInt(guess.userGuess)) {
+
+        // Subtract the damage amount from the zombie's health.
+        zombieHealth -= damage;
+        console.log("YOU HIT THE ZOMBIE WITH " + damage + " damage");
+        console.log("You have " + userHealth + " health left. The Zombie has " + zombieHealth + " health left.");
+
+        // Check if the game is over.
+        checkRound();
+      }
+
+      else {
+        // Subtract the damage amount from the user's health.
+        userHealth -= damage;
+        console.log("OH NO! The zombie slashed you with " + damage + " damage");
+        console.log("You have " + userHealth + " health left. The Zombie has " + zombieHealth + " health left.");
+
+        // Check if the game is over.
+        checkRound();
+
+      }
+    }
+  });
+}
+
+// Starts the game!
+playRound();
